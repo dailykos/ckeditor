86820dfbe417a13a7a689a195eb4c22e577db852
Scott Gonyea <me@sgonyea.com>
2015-06-11 14:23:46 -0700

    re-use the hash from the current JS file

--- app/assets/javascripts/ckeditor/init.js
+++ app/assets/javascripts/ckeditor/init.js
@@ -5,4 +5,24 @@
   if (typeof window['CKEDITOR_BASEPATH'] === "undefined" || window['CKEDITOR_BASEPATH'] === null) {
     window['CKEDITOR_BASEPATH'] = "/assets/ckeditor/";
   }
+
+  // Just assume the JS file is being minified and contains a trailing hash (eg, application-<hash>.js):
+  if(! window['CKEDITOR_CBUSTER']) {
+    var scriptHash  = '';
+
+    try {
+      if(document.currentScript) {
+        var cScriptSrc  = document.currentScript.src,
+            fileName    = cScriptSrc.split("/").pop();
+
+        fileName = fileName.split(".js").shift();
+        fileName = fileName.split("-").pop();
+
+        if(fileName) { scriptHash = fileName; }
+      }
+    } catch(e) { /* meh */ }
+
+    window['CKEDITOR_CBUSTER'] = scriptHash;
+  }
+
 }).call(this);
